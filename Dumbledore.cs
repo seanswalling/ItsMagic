60d178b1038f9c9be7cb0bb69f12c49f5f890e51
Jordan466 <jordan.warren@cashconverters.com>
2017-03-08 17:05:27 +0800

    added stuff

--- Dumbledore.cs
+++ Dumbledore.cs
@@ -2,30 +2,82 @@
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace ItsMagic
 {
     static class Dumbledore
     {
-        static public void UpdateProjectReferenceWithNugetReference(string csProjFileToUpdate, CsProj projectReferenceToRemove,
+        public static void UpdateProjectReferenceWithNugetReference(string csProjFileToUpdate, CsProj projectReferenceToRemove,
             NugetPackageReference referenceToAdd)
         {
             
         }
 
+        public static void UpdateReference(string csProj, Reference reference)
+        {
+            //Find and Delete old Reference
+            RemoveReference(csProj, reference);
+            //Find <ItemGroup> - string replace <ItemGroup> with <ItemGroup>+Reference Info
+            AddNugetReference(csProj, reference);
+            reformatXML(csProj);
+            UpdatePackagesConfig(Path.GetDirectoryName(csProj) + "\\packages.config", reference);
+        }
+
+        public static void RemoveReference(string csProj, Reference reference)
+        {
+            var Regex = new Regex(reference.ProjectRefPattern);
+            var csProjText = File.ReadAllText(csProj);
+            csProjText = Regex.Replace(csProjText, "", 1);
+            File.WriteAllText(csProj, csProjText);
+        }
+
+        public static void AddNugetReference(string csProj, Reference reference)
+        {
+            var Regex = new Regex(itemGroup);
+            var csProjText = File.ReadAllText(csProj);
+            csProjText = Regex.Replace(csProjText, itemGroup + reference.NugetRef, 1);
+            File.WriteAllText(csProj, csProjText);
+        }
+
+        private static void reformatXML(string file)
+        {
+            var doc = XDocument.Load(file);
+            using (XmlTextWriter writer = new XmlTextWriter(file, System.Text.Encoding.UTF8))
+            {
+                writer.Formatting = Formatting.Indented;
+                doc.Save(writer);
+            }
+        }
+
+        private static void UpdatePackagesConfig(string packages, Reference reference)
+        {
+            var Regex = new Regex(packagesTag);
+            CreateIfDoestExist(packages);
+            var packagesText = File.ReadAllText(packages);
+            packagesText = Regex.Replace(packagesText, packagesTag + reference.PackagesRef, 1);
+            File.WriteAllText(packages, packagesText);
+            reformatXML(packages);
+        }
+
+        private static void CreateIfDoestExist(string packages)
+        {
+            if (!File.Exists(packages))
+            {
+                File.WriteAllText(packages, "<?xml version=\"1.0\" encoding=\"utf-8\"?><packages></packages>");
+            }
+        }
 
 
 
 
-
-
-
-        static public IEnumerable<string> GetSlnFiles(string projectDirectory)
+        public static IEnumerable<string> GetSlnFiles(string projectDirectory)
         {
             return Directory.EnumerateFiles(projectDirectory, "*.sln", SearchOption.AllDirectories);
         }
 
-        static public IEnumerable<string> GetCsProjs(string sln)
+        public static IEnumerable<string> GetCsProjs(string sln)
         {
             var dir = Path.GetDirectoryName(sln);
             var regex = new Regex("Project(.*) = .*, \"(?<proj>.*\\.csproj)\", \".*\"");
@@ -36,7 +88,7 @@ namespace ItsMagic
                 .Select(relPath => Path.Combine(dir, relPath));
         }
 
-        static public IEnumerable<string> GetCsFiles(string csproj)
+        public static IEnumerable<string> GetCsFiles(string csproj)
         {
             var dir = Path.GetDirectoryName(csproj);
             var regex = new Regex("(?:<Compile Include=\\\")(?<cs>(?:(?:\\w+)\\\\*(?:\\w+))*\\.cs)(?:\\\" \\/>)");
